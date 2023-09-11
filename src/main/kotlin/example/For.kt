7402f62a31e36f17c3b3050a6be278a07f4dd2d3
yunyoung1819 <yunyoung1819@hanmail.net>
2023-09-12 03:01:18 +0900

    :sparkles: feature: 흐름제어

--- src/main/kotlin/example/For.kt
+++ src/main/kotlin/example/For.kt
@@ -2,19 +2,26 @@ package example
 
 fun main() {
 
-    // 범위 연산자 ..를 사용해 for loop 돌리기
+    /**
+     * 범위 연산자 ..를 사용해 for loop 돌리기
+     */
     for (i in 0 .. 3) {
-        println(i)
+        // println(i)
     }
 
-//    for (i in 1 .. 9) {
-//        print(i)
-//        print(" ")
-//    }
-
-    // until을 사용해 반복한다
-    // 뒤에 온 숫자는 포함하지 않는다
+    /**
+     * until을 사용해 반복한다
+     * 뒤에 온 숫자는 포함하지 않는다
+     */
     for (i in 0 until 3) {
+        // println(i)
+    }
+
+    /**
+     * step에 들어온 값 만큼 증가시킨다
+     */
+    for (i in 0 .. 10 step 2) {
         println(i)
     }
+
 }
