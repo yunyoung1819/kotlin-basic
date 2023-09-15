f81b5b86b65ef9a47590afbc6fcba605278c4c9b
yunyoung1819 <yunyoung1819@hanmail.net>
2023-09-15 22:52:59 +0900

    :sparkles: feature: 코틀린 예외처리

---
+++ src/main/kotlin/example/java/Java_Exception.java
@@ -0,0 +1,12 @@
+package example.java;
+
+public class Java_Exception {
+
+	public static void main(String[] args) {
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+			// 예외 처리
+		}
+	}
+}
