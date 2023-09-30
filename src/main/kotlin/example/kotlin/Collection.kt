94377ec14d94ec2aac56661aad6f9aa22c8599f5
yunyoung1819 <yunyoung1819@hanmail.net>
2023-09-30 14:31:10 +0900

    :sparkles: feature: kotlin study Collection.kt

--- src/main/kotlin/example/kotlin/Collection.kt
+++ src/main/kotlin/example/kotlin/Collection.kt
@@ -1,11 +1,20 @@
 package example.kotlin
 
+import java.util.*
+
+/**
+ * Collection
+ */
 fun main() {
 
-    // immutable
+    /**
+     * immutable
+     */
     val currencyList = listOf("달러", "유로", "원")
 
-    // mutable
+    /**
+     * mutable
+     */
 //    val mutableCurrencyList = mutableListOf<String>()
 //    mutableCurrencyList.add("달러")
 //    mutableCurrencyList.add("유로")
@@ -15,5 +24,66 @@ fun main() {
         add("달러")
         add("유로")
         add("원")
+    }
+
+    /**
+     * immutable set
+     */
+    val numberSet = setOf(1, 2, 3, 4)
+
+    /**
+     * mutable set
+     */
+    val mutableSet = mutableSetOf<Int>().apply {
+        add(1)
+        add(2)
+        add(3)
+        add(4)
+    }
+
+    /**
+     * immutable map
+     */
+    val numberMap = mapOf("one" to 1, "two" to 2)
+
+    /**
+     * mutable map
+     */
+    val mutableNumberMap = mutableMapOf<String, Int>()
+    mutableNumberMap["one"] = 1
+    mutableNumberMap["two"] = 2
+    mutableNumberMap["three"] = 3
+
+    /**
+     * 컬렉션 빌더는 내부에선 mutable 반환은 immutable
+     */
+    val numberList = buildList{
+        add(1)
+        add(2)
+        add(3)
+        add(4)
+    }
+
+    /**
+     * linkedList
+     */
+    val linkedList = LinkedList<Int>().apply {
+        addFirst(3)
+        add(2)
+        addLast(1)
+    }
+
+    /**
+     * arrayList
+     */
+    val arrayList = ArrayList<Int>().apply {
+        add(1)
+        add(2)
+        add(3)
+    }
+
+    val iterator = currencyList.iterator()
+    while (iterator.hasNext()) {
+        println(iterator.next())
     }
 }
